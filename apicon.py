1ea79524cfd68e1098a20adb3ce69dab9451c88f
oaraujor <149982899+oaraujor@users.noreply.github.com>
2023-11-10 08:55:26 -0600

    Update apicon.py

--- apicon.py
+++ apicon.py
@@ -1,15 +1,14 @@
 import requests
 from datetime import datetime
 
-fechaHoy = datetime.now().date()
-fechaHoy = fechaHoy.strftime('%d-%m-%Y')
-archivoNom = 'datosAPI\Data-' + fechaHoy +'.txt'
-
 def writeFILE(texto, archivo):
     with open(archivo, 'w') as file:
         file.write(str(texto))
 
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
+fechaHoy = datetime.now().date()
+fechaHoy = fechaHoy.strftime('%d-%m-%Y')
+archivoNom = 'datosAPI\Data-' + fechaHoy +'.
+
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 r = requests.get(url)
 data = r.json()
