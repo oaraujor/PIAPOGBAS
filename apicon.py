3971cf49dd7886d0e00f49d627d00c7f27d3ff70
Octavio Araujo <oaraujor_@hotmail.com>
2023-11-11 16:47:51 -0600

    agregado el menu de main, pendientes las funciones

--- apicon.py
+++ apicon.py
@@ -1,16 +1,21 @@
 import requests
 from datetime import datetime
 
-fechaHoy = datetime.now().date()
-fechaHoy = fechaHoy.strftime('%d-%m-%Y')
-archivoNom = 'datosAPI\Data-' + fechaHoy +'.txt'
-
 def writeFILE(texto, archivo):
     with open(archivo, 'w') as file:
         file.write(str(texto))
+
+fechaHoy = datetime.now().date()
+fechaHoy = fechaHoy.strftime('%d-%m-%Y')
+archivoNom = 'datosAPI\Data-' + fechaHoy +'.txt'
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 r = requests.get(url)
 data = r.json()
-writeFILE(data,archivoNom)
+
+keys = data.keys()
+for i in keys:
+    print(str(data[i]))
+
+# writeFILE(data,archivoNom) #escritura de texto
