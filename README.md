e30f3f83f2d34c3c0899cb690166fc047128979b
Octavio Araujo <oaraujor_@hotmail.com>
2023-11-11 17:22:51 -0600

    worked on readme

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
-MATERIA: PROGRAMACION BASICA
-SEMESTRE: JUNIO - DICIEMBRE 2023
-ALUMNOS: OCTAVIO ARAUJO ROSALES 2173394
-         SERGIO ARMANDO GARZA TREVIÑO 1986186
-         REGINA HERNANDEZ RODRIGUEZ 2000640
+# EVIDENCIA:  Producto Integrador de Aprendizaje (PIA)  
+## OBJETIVOS:  
+### - Implementar los diversos temas vistos en la UA para realizar un proyecto en Python que permita analizar información y extraer datos significativos de forma automatizada.  
+## MATERIA
+### - PROGRAMACION BASICA  
+## SEMESTRE
+### JUNIO - DICIEMBRE 2023
 
-API PRINCIPAL: 
+## ALUMNOS:
+### - OCTAVIO ARAUJO ROSALES 2173394
+### - SERGIO ARMANDO GARZA TREVIÑO 1986186
+### - REGINA HERNANDEZ RODRIGUEZ 2000640
--- apicon.py
+++ apicon.py
@@ -1,20 +1,18 @@
 import requests
+import json
 from datetime import datetime
 
+# writeFILE(data,archivoNom) #escritura de texto
 def writeFILE(texto, archivo):
     with open(archivo, 'w') as file:
         file.write(str(texto))
+    
 
 fechaHoy = datetime.now().date()
 fechaHoy = fechaHoy.strftime('%d-%m-%Y')
-<<<<<<< HEAD
 archivoNom = 'datosAPI\Data-' + fechaHoy +'.txt'
 
 # replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-=======
-archivoNom = 'datosAPI\Data-' + fechaHoy +'.
-
->>>>>>> 1ea79524cfd68e1098a20adb3ce69dab9451c88f
 url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=demo'
 r = requests.get(url)
 data = r.json()
@@ -23,4 +21,4 @@ keys = data.keys()
 for i in keys:
     print(str(data[i]))
 
-# writeFILE(data,archivoNom) #escritura de texto
+
